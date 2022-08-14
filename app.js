02b132b07b27dd7d72ba1fa03d64120dad0d83a0
Portillo10 <portillon02@gmail.com>
2022-08-14 15:40:16 -0500

    Server con express

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const hbs = require('hbs')
+require('dotenv').config()
+
+const app = express()
+const port = process.env.PORT
+
+app.set('view engine', 'hbs')
+hbs.registerPartials(__dirname + '/views/partials')
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+    res.render('index',{
+        name: 'Potrancas',
+        title: 'SolarTec'
+    })
+})
+
+app.get('/generic', (req, res) => {
+    res.render('generic',{
+        name: 'Potrancas',
+        title: 'SolarTec'
+    })
+})
+
+app.get('/elements', (req, res) => {
+    res.render('elements',{
+        name: 'Potrancas',
+        title: 'SolarTec'
+    })
+})
+
+app.listen(port)
